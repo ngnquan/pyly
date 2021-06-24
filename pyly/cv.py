da13df49d086cbfcfcca910d6d38e7cee28eadb9
Quan Nguyen (Fly) <ngnamquan@gmail.com>
2021-06-24 23:40:29 +0700

    Update cv.py

--- pyly/cv.py
+++ pyly/cv.py
@@ -58,3 +58,61 @@ def compute_overlap_y(bbox1, bbox2, crite=min):
         return 0
 
     return dx / crite(bbox1[3]-bbox1[1], bbox2[3]-bbox2[1])
+
+def shrink_edge(xy_list, new_xy_list, edge, r, theta, ratio=0.3):
+    if ratio == 0.0:
+        return
+    start_point = edge
+    end_point = (edge + 1) % 4
+    long_start_sign_x = np.sign(
+        xy_list[end_point, 0] - xy_list[start_point, 0])
+    new_xy_list[start_point, 0] = \
+        xy_list[start_point, 0] + \
+        long_start_sign_x * ratio * r[start_point] * np.cos(theta[start_point])
+    long_start_sign_y = np.sign(
+        xy_list[end_point, 1] - xy_list[start_point, 1])
+    new_xy_list[start_point, 1] = \
+        xy_list[start_point, 1] + \
+        long_start_sign_y * ratio * r[start_point] * np.sin(theta[start_point])
+    
+    # long edge one, end point
+    long_end_sign_x = -1 * long_start_sign_x
+    new_xy_list[end_point, 0] = \
+        xy_list[end_point, 0] + \
+        long_end_sign_x * ratio * r[end_point] * np.cos(theta[start_point])
+    long_end_sign_y = -1 * long_start_sign_y
+    new_xy_list[end_point, 1] = \
+        xy_list[end_point, 1] + \
+        long_end_sign_y * ratio * r[end_point] * np.sin(theta[start_point])
+
+def shrink(xy_list, ratio=0.3, epsilon=1e-4):
+    if ratio == 0.0:
+        return xy_list, xy_list
+    
+    diff_1to3 = xy_list[:3, :] - xy_list[1:4, :]
+    diff_4 = xy_list[3:4, :] - xy_list[0:1, :]
+    diff = np.concatenate((diff_1to3, diff_4), axis=0)
+    dis = np.sqrt(np.sum(np.square(diff), axis=-1))
+    
+    # determine which are long or short edges
+    long_edge = int(np.argmax(np.sum(np.reshape(dis, (2, 2)), axis=0)))
+    short_edge = 1 - long_edge
+    # cal r length array
+    r = [np.minimum(dis[i], dis[(i + 1) % 4]) for i in range(4)]
+    
+    # cal theta array
+    diff_abs = np.abs(diff)
+    diff_abs[:, 0] += epsilon
+    theta = np.arctan(diff_abs[:, 1] / diff_abs[:, 0])
+    
+    # shrink two long edges
+    temp_new_xy_list = np.copy(xy_list)
+    shrink_edge(xy_list, temp_new_xy_list, long_edge, r, theta, ratio)
+    shrink_edge(xy_list, temp_new_xy_list, long_edge + 2, r, theta, ratio)
+    
+    # shrink two short edges
+    new_xy_list = np.copy(temp_new_xy_list)
+    shrink_edge(temp_new_xy_list, new_xy_list, short_edge, r, theta, ratio)
+    shrink_edge(temp_new_xy_list, new_xy_list, short_edge + 2, r, theta, ratio)
+    
+    return temp_new_xy_list, new_xy_list, long_edge
